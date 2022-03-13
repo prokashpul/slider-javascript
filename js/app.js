22f566701153b759c6d4b0cbb7e389c2445429d9
Prokash Pul <pprokash102@gmail.com>
2022-03-13 07:44:33 +0600

    left arrow click event add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-const slider = document.querySelectorAll('.slider-img')
-const dotNav= document.querySelectorAll('.dot')
-const rightArrow = document.querySelector('.right-arrow')
+const slider = document.querySelectorAll('.slider-img');
+const dotNav= document.querySelectorAll('.dot');
+const rightArrow = document.querySelector('.right-arrow');
+const leftArrow = document.querySelector('.left-arrow');
 
 let currentSidler = 1;
 
@@ -8,27 +9,27 @@ let currentSidler = 1;
 
 const dotNavigation =(manual)=>{
     slider.forEach((slide)=>{
-        slide.classList.remove('active')
+        slide.classList.remove('active');
         dotNav.forEach((dot)=>{
-            dot.classList.remove('active')
+            dot.classList.remove('active');
         });
     });
 
-    slider[manual].classList.add('active')
-    dotNav[manual].classList.add('active')
+    slider[manual].classList.add('active');
+    dotNav[manual].classList.add('active');
 }
 
 // dote click event add
 dotNav.forEach((dot,i)=>{
     dot.addEventListener('click',()=>{
-        dotNavigation(i)
+        dotNavigation(i);
         currentSidler=i;
     });
 });
 // slide
 let sliderNum = 0;
 const sliderSlide = ()=>{
-    sliderNum++
+    sliderNum++;
     if(slider.length<=sliderNum){
         sliderNum = 0;
         dotNavigation(sliderNum);
@@ -37,5 +38,15 @@ const sliderSlide = ()=>{
     }
 }
 rightArrow.addEventListener('click',()=>{
-    sliderSlide()
+    sliderSlide();
+});
+// left arrow button
+leftArrow.addEventListener('click',()=>{
+    sliderNum--;
+    if(sliderNum<0){
+        sliderNum = slider.length - 1 ;
+        dotNavigation(sliderNum);
+    }else{
+        dotNavigation(sliderNum);
+    }
 })
